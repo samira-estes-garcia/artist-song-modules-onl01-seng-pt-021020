ba3ec84b6b662dbd8be513fcf6ae29813b4a24c2
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-08 23:43:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
 
 require 'pry'
 
 class Song  
   
   extend Memorable
+  extend Findable
   
   attr_accessor :name
   attr_reader :artist
@@ -15,9 +17,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+ # def self.find_by_name(name)
+  #  @@songs.detect{|a| a.name == name}
+ # end
 
  def self.all
     @@songs
